875ed361b4f0151e792f204debf5feb4bf952600
Austin <alomax@ggc.edu>
2022-01-12 15:07:08 -0500

    added files

--- MovieShopMVC/Controllers/HomeController.cs
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApplicationCore.Contracts.Services;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
 using MovieShopMVC.Models;
 using System.Diagnostics;
 
@@ -7,21 +9,44 @@ namespace MovieShopMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IMovieService _movieService;
+        public HomeController(ILogger<HomeController> logger, IMovieService movieService)
         {
             _logger = logger;
+            _movieService = movieService;
         }
 
-        public IActionResult Index()
+
+        //T1 Synchrous code
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            ///some code
+            // call Movie Service to get topgrossingmovies
+            // 3 ways we can pass the data from COntroller/Action methods to the Views
+            // 1. *** Pass the Strongly Typed Models ***
+            // 2. ViewBag => dynamic
+            // 3. ViewData => object key/value
+            // you can await any method that returns Task
+
+            var movies = await _movieService.GetTop30GrossingMovies();
+            ViewBag.TotalMovies = movies.Count;
+            return View(movies);
         }
 
+        [HttpGet]
         public IActionResult Privacy()
         {
             return View();
         }
+
+        // http://localhost:53535/Home/TopMovies
+        [HttpGet]
+        public IActionResult TopMovies()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
