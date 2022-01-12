e33bbb18f5f4ae0307b3b5f878b328216514e9a1
Austin <alomax@ggc.edu>
2022-01-12 15:34:24 -0500

    updated and created files

--- ApplicationCore/Contracts/Services/IMovieService.cs
+++ ApplicationCore/Contracts/Services/IMovieService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApplicationCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace ApplicationCore.Contracts.Services
 {
     public interface IMovieService
     {
+        // create these service methods absed on your UI/business requirements
+        // Controllers will access these methods
+        Task<List<MovieCardResponseModel>> GetTop30GrossingMovies();
+        Task<MovieDetailsResponseModel> GetMovieDetails(int id);
     }
 }
---
+++ Infrastructure/Services/MovieService.cs
@@ -0,0 +1,62 @@
+﻿using ApplicationCore.Contracts.Repoisitories;
+using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public async Task<MovieDetailsResponseModel> GetMovieDetails(int id)
+        {
+            var movieDetails = await _movieRepository.GetById(id);
+
+            var movieModel = new MovieDetailsResponseModel
+            {
+                Id = movieDetails.Id,
+                Title = movieDetails.Title,
+                PosterUrl = movieDetails.PosterUrl,
+                BackdropUrl = movieDetails.BackdropUrl,
+                ImdbUrl = movieDetails.ImdbUrl
+            };
+
+            foreach (var genre in movieDetails.GernesOfMovie)
+            {
+                movieModel.Genres.Add(new GenreModel { Id = genre.GenreId, Name = genre.Genre.Name });
+            }
+
+            foreach (var trailer in movieDetails.Trailers)
+            {
+                movieModel.Trailers.Add(new TrailerModel { Id = trailer.Id, Name = trailer.Name, TrailerUrl = trailer.TrailerUrl });
+            }
+
+            return movieModel;
+        }
+
+        public async Task<List<MovieCardResponseModel>> GetTop30GrossingMovies()
+        {
+            var movies = await _movieRepository.Get30HighestGrossingMovies();
+            // map the data from movies (List<Movie>) to movieCards (List<MovieCardResponseModel>)
+
+            var movieCards = new List<MovieCardResponseModel>();
+
+            foreach (var movie in movies)
+            {
+                movieCards.Add(new MovieCardResponseModel { Id = movie.Id, Title = movie.Title, PosterUrl = movie.PosterUrl });
+            }
+
+            return movieCards;
+        }
+    }
+}
